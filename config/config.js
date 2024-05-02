2c7fdb4d21dea574a8b6d35c792269e141b74ec9
Emily Wu <86927881+em0227@users.noreply.github.com>
2024-05-02 11:06:13 -0400

    refactor to use knex for db instead

--- config/config.js
+++ config/config.js
@@ -1,18 +1,7 @@
-// PROMISE LIBRARY
-const promise = require("bluebird");
-
-// OVERRIDING DEFAULT PROMISE LIBRARY
-const options = {
-  promiseLib: promise,
-  query: (e) => {
-    console.log(e.query);
-  },
-  // rejectUnauthorized: false,
-};
-const pgp = require("pg-promise")(options);
-
-// SET UP THE CONNECTION STRING FOR THE DATABASE
-const connectionString = process.env.DATABASE_URL;
-const db = pgp(connectionString);
+const db = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  searchPath: ['knex', 'public'],
+});
 
 module.exports = db;
--- routes/api/tickets.js
+++ routes/api/tickets.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 const db = require("../../config/config");
 
 router.get("/test", (req, res) => {
@@ -7,48 +8,50 @@ router.get("/test", (req, res) => {
 });
 
 //TODO
-router.get("/all");
+router.get("/all", (req, res) => {
+    db.any()
+});
 
 //TODO
 //should also return all the replies associated to the ticket
 router.get("/:id");
 
-//TODO: check bluebird docs
 router.post("/", (req, res) => {
+    const id = uuidv4();
     const status = 'new';
     const { createdBy, email, title, description, created } = req.body;
-    return db
-        .none(
-            `INSERT into tickets(id, created_by, email, title, description, status, created)
-             VALUES($1, $2, $3, $4, $5, $6, $7)`,
-            [id, createdBy, email, title, description, status, created]
-        )
-        .then(() => {
-            res.json({ success: true }).status(200);
+    try {
+        await db('tickets').insert({
+            id,
+            status,
+            email,
+            title,
+            description,
+            created,
+            created_by: createdBy
         })
-        .catch((err) => {
-            res.json({ err }).status(500);
-        });
+    } catch (err) {
+        res.json({ err }).status(500);
+    }
 });
 
-//should the ticket be updated if an reply was added but nothing changed on the ticket?
+//TODO: should the ticket be updated if an reply was added but nothing changed on the ticket?
 router.patch("/", (req, res) => {
     const { ticketID, email, title, description, status, updatedBy, updated } = req.body;
 
-    db.none(`UPDATE tickets 
-        SET email = COALESCE($2, email),
-        title = COALESCE($3, title),
-        description = COALESCE($4, description),
-        status = COALESCE($5, status),
-        updated_by = $6,
-        updated = $7,
-        WHERE id = $1`, [ticketID, email, title, description, status, updatedBy, updated])
-        .then(() => {
-            res.json({ success: true }).status(200);
+    try {
+        db('tickets').where({ id: ticketID }).update({
+            updated,
+            updated_by: updatedBy,
+            ...email ? { email } : {},
+            ...title ? { title } : {},
+            ...description ? { description } : {},
+            ...status ? { status } : {}
         })
-        .catch((err) => {
-            res.json({ err }).status(500);
-        });
+    } catch (err) {
+        res.json({ err }).status(500);
+    }
+
 });
 
 module.exports = router;
