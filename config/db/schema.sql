fbfe914fdc801d4cecd9657d59ac20dc7e6d44f7
Emily Wu <86927881+em0227@users.noreply.github.com>
2024-05-02 08:40:32 -0400

    create database schema and dummy data

---
+++ config/db/schema.sql
@@ -0,0 +1,35 @@
+
+DROP DATABASE IF EXISTS jila;
+CREATE DATABASE jila;
+
+\c jila;
+
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS tickets;
+
+CREATE TABLE tickets(
+    id INT PRIMARY KEY NOT NULL,
+    created_by VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    description TEXT,
+    status VARCHAR NOT NULL,
+    created TIMESTAMP NOT NULL,
+    updated_by VARCHAR,
+    updated TIMESTAMP
+);
+
+INSERT into tickets(id, created_by, email, title, description, status, created) 
+VALUES('1', 'emily', 'emily@test.com', 'need reset password', 'forgot password need reset link', 'new', NOW());
+
+CREATE TABLE replies(
+    id INT PRIMARY KEY NOT NULL,
+    ticket_id INT NOT NULL,
+    response TEXT NOT NULL,
+    created_by VARCHAR NOT NULL,
+    created TIMESTAMP NOT NULL,
+    CONSTRAINT fk_ticket_id FOREIGN KEY (ticket_id) REFERENCES tickets(id)
+);
+
+INSERT into replies(id, ticket_id, created_by, response, created) 
+VALUES('1', '1', 'admin', 'solved', NOW())
