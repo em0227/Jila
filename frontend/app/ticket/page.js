15cc100bca1593ebc978b191060b51057d7b2f63
Emily Wu <86927881+em0227@users.noreply.github.com>
2024-05-03 22:48:23 -0400

    form look complete

--- frontend/app/ticket/page.js
+++ frontend/app/ticket/page.js
@@ -1,16 +1,22 @@
 "use client";
 
 import React from "react";
-import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { TextInput, Button, Group, Box, Textarea } from "@mantine/core";
 import { useForm } from "@mantine/form";
 
+//TODO create utils to fire requsets
+//TODO admin should be able to update ticket in id page
+//TODO admin can add replies
+//TODO write test for create ticket & validation
+//TODO write test only admin can see ticket list
 const Ticket = () => {
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
       title: "",
       email: "",
-      termsOfService: false,
+      author: "",
+      description: "",
     },
 
     validate: {
@@ -19,24 +25,42 @@ const Ticket = () => {
   });
 
   return (
-    <div className="mt-5 flex">
+    <div className="mt-5">
       <h1 className="text-lg font-bold">Ticket Page</h1>
 
-      <Box maw={340} mx="auto">
+      <Box maw={340}>
         <form onSubmit={form.onSubmit((values) => console.log(values))}>
           <TextInput
             withAsterisk
-            label="Ticket Title"
+            label="Title"
             placeholder="I can't reset my password"
             key={form.key("title")}
             {...form.getInputProps("title")}
           />
 
-          <Checkbox
-            mt="md"
-            label="I agree to sell my privacy"
-            key={form.key("termsOfService")}
-            {...form.getInputProps("termsOfService", { type: "checkbox" })}
+          <TextInput
+            withAsterisk
+            label="Author"
+            placeholder="Your name"
+            key={form.key("author")}
+            {...form.getInputProps("author")}
+          />
+
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="Your email"
+            key={form.key("email")}
+            {...form.getInputProps("email")}
+          />
+
+          <Textarea
+            placeholder="Describe your issue in details"
+            label="Description"
+            autosize
+            minRows={2}
+            key={form.key("description")}
+            {...form.getInputProps("description")}
           />
 
           <Group justify="flex-end" mt="md">
