cdc327e1a3440a7028b489c821c51379bf9dc643
Emily Wu <86927881+em0227@users.noreply.github.com>
2024-05-05 17:34:47 -0400

    change port

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get("/test", (req, res) => {
 app.use("/api/tickets", tickets);
 app.use("/api/replies", replies);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
